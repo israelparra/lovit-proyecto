536adaecfdf3a8d911ac98f652849be4d1425550
israelparra <israelparracamara@gmail.com>
2023-03-22 16:56:52 -0700

    ultimo

--- proyecto-lovit/src/main/java/proyectolovit/proyectolovit/model/Producto.java
+++ proyecto-lovit/src/main/java/proyectolovit/proyectolovit/model/Producto.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 
 @Entity
@@ -18,6 +15,7 @@ public class Producto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_producto")
     private int id;
 
     private String nombre;
