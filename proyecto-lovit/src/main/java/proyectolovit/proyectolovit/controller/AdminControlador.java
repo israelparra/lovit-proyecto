1270c49f5d460b004a870667093559f719ec9dd0
israelparra <israelparracamara@gmail.com>
2023-03-21 22:57:38 -0700

    cambios

--- proyecto-lovit/src/main/java/proyectolovit/proyectolovit/controller/AdminControlador.java
+++ proyecto-lovit/src/main/java/proyectolovit/proyectolovit/controller/AdminControlador.java
@@ -1,19 +1,15 @@
 package proyectolovit.proyectolovit.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import proyectolovit.proyectolovit.model.Imagen;
 import proyectolovit.proyectolovit.model.Producto;
 import proyectolovit.proyectolovit.repositories.ProductoRepositorio;
 import proyectolovit.proyectolovit.services.AlmacenServicio;
@@ -24,26 +20,23 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminControlador {
 
-    @Autowired
-    private ProductoRepositorio productoRepositorio;
+//    @Autowired
+//    private ProductoRepositorio productoRepositorio;
 
-    @Autowired
-    private AlmacenServicio servicio;
+//    @GetMapping("")
+//    public ModelAndView verPaginaDeInicio(@PageableDefault(sort = "nombre", size = 5) Pageable pageable) {
+//        Page<Producto> productos = productoRepositorio.findAll(pageable);
+//        return new ModelAndView("index").addObject("productos", productos);
+//    }
+//
+//    @GetMapping("/productos/nuevo")
+//    public ModelAndView mostrarFormularioDeNuevoProducto() {
+//        List<Producto> productos = productoRepositorio.findAll(Sort.by("nombre"));
+//        return new ModelAndView("admin/nuevo-producto")
+//                .addObject("producto", new Producto());
+//    }
 
-    @GetMapping("")
-    public ModelAndView verPaginaDeInicio(@PageableDefault(sort = "titulo", size = 5) Pageable pageable) {
-        Page<Producto> productos = productoRepositorio.findAll(pageable);
-        return new ModelAndView("admin/index").addObject("productos", productos);
-    }
-
-    @GetMapping("/productos/nuevo")
-    public ModelAndView mostrarFormularioDeNuevoProducto() {
-        List<Producto> productos = productoRepositorio.findAll(Sort.by("nombre"));
-        return new ModelAndView("admin/nuevo-producto")
-                .addObject("producto", new Producto());
-    }
-
-    @PostMapping("/productos/nuevo")
+    /**@PostMapping("/productos/nuevo")
     public ModelAndView registrarProducto(@Validated Imagen imagen, Producto producto,BindingResult bindingResult) {
         if(bindingResult.hasErrors() || imagen.getPortada().isEmpty()) {
             if(imagen.getPortada().isEmpty()) {
--- proyecto-lovit/src/main/java/proyectolovit/proyectolovit/model/Producto.java
+++ proyecto-lovit/src/main/java/proyectolovit/proyectolovit/model/Producto.java
@@ -1,196 +1,47 @@
 package proyectolovit.proyectolovit.model;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Producto {
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    @NotBlank
     private String nombre;
 
-    @NotBlank
     private String descripcion;
 
-    @NotNull
     private float precio;
 
-    @NotNull
-    private int minimoCompra;
+    private int minimo_compra;
 
-    @NotNull
     private float alto;
 
-    @NotNull
     private float ancho;
 
-    @NotNull
     private float peso;
 
-    @NotBlank
-    private Tag tags;
+    private int tags;
 
-    @NotBlank
-    private Imagen imagenPortada;
+    private long imagen_portada;
 
-    @NotEmpty
-    private ArrayList<Imagen> imagenesAdicionales;
+    private ArrayList<Imagen> imagenes_adicionales;
 
-    @NotEmpty
     private ArrayList<InsumoProducto> insumosReq;
 
-    public Producto() {
-    }
 
-    public Producto(long id, @NotBlank String nombre, @NotBlank String descripcion, @NotNull float precio,
-                    @NotNull int minimoCompra, @NotNull float alto, @NotNull float ancho, @NotNull float peso,
-                    @NotBlank Tag tags, @NotBlank Imagen imagenPortada, @NotEmpty ArrayList<Imagen> imagenesAdicionales,
-                    @NotEmpty ArrayList<InsumoProducto> insumosReq) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.minimoCompra = minimoCompra;
-        this.alto = alto;
-        this.ancho = ancho;
-        this.peso = peso;
-        this.tags = tags;
-        this.imagenPortada = imagenPortada;
-        this.imagenesAdicionales = imagenesAdicionales;
-        this.insumosReq = insumosReq;
-    }
 
-    public Producto(@NotBlank String nombre, @NotBlank String descripcion, @NotNull float precio,
-                    @NotNull int minimoCompra, @NotNull float alto, @NotNull float ancho, @NotNull float peso,
-                    @NotBlank Tag tags, @NotBlank Imagen imagenPortada, @NotEmpty ArrayList<Imagen> imagenesAdicionales,
-                    @NotEmpty ArrayList<InsumoProducto> insumosReq) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.minimoCompra = minimoCompra;
-        this.alto = alto;
-        this.ancho = ancho;
-        this.peso = peso;
-        this.tags = tags;
-        this.imagenPortada = imagenPortada;
-        this.imagenesAdicionales = imagenesAdicionales;
-        this.insumosReq = insumosReq;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public float getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(float precio) {
-        this.precio = precio;
-    }
-
-    public int getMinimoCompra() {
-        return minimoCompra;
-    }
-
-    public void setMinimoCompra(int minimoCompra) {
-        this.minimoCompra = minimoCompra;
-    }
-
-    public float getAlto() {
-        return alto;
-    }
-
-    public void setAlto(float alto) {
-        this.alto = alto;
-    }
-
-    public float getAncho() {
-        return ancho;
-    }
-
-    public void setAncho(float ancho) {
-        this.ancho = ancho;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
-
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-
-    public Tag getTags() {
-        return tags;
-    }
-
-    public void setTags(Tag tags) {
-        this.tags = tags;
-    }
-
-    public Imagen getImagenPortada() {
-        return imagenPortada;
-    }
-
-    public void setImagenPortada(Imagen imagenPortada) {
-        this.imagenPortada = imagenPortada;
-    }
-
-    public ArrayList<Imagen> getImagenesAdicionales() {
-        return imagenesAdicionales;
-    }
-
-    public void setImagenesAdicionales(ArrayList<Imagen> imagenesAdicionales) {
-        this.imagenesAdicionales = imagenesAdicionales;
-    }
-
-    public ArrayList<InsumoProducto> getInsumosReq() {
-        return insumosReq;
-    }
-
-    public void setInsumosReq(ArrayList<InsumoProducto> insumosReq) {
-        this.insumosReq = insumosReq;
-    }
-
-    @Override
-    public String toString() {
-        return "Producto{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                ", precio=" + precio +
-                ", minimoCompra=" + minimoCompra +
-                ", alto=" + alto +
-                ", ancho=" + ancho +
-                ", peso=" + peso +
-                ", tags=" + tags +
-                ", imagenPortada=" + imagenPortada +
-                ", imagenesAdicionales=" + imagenesAdicionales +
-                ", insumosReq=" + insumosReq +
-                '}';
-    }
 }
