91c2ef4699e0d96d872024a0658fd8f4bcfdfa6c
AndreaASC <andrea.salazar229333@potros.itson.edu.mx>
2023-04-19 14:15:51 -0700

    funcionalidad de busqueda añadida

--- lovit-proyecto-master/lovit-proyecto-version1/proyecto-lovit/src/main/java/proyectolovit/proyectolovit/repositories/ProductoRepositorio.java
+++ lovit-proyecto-master/lovit-proyecto-version1/proyecto-lovit/src/main/java/proyectolovit/proyectolovit/repositories/ProductoRepositorio.java
@@ -1,7 +1,11 @@
 package proyectolovit.proyectolovit.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import proyectolovit.proyectolovit.model.Producto;
 
+import java.util.List;
+
 public interface ProductoRepositorio  extends JpaRepository<Producto, Integer> {
+    List<Producto> findByNombreContaining(String nombre);
 }
